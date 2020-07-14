f1f55e678b8da5b11c804105cf64ff8089dae398
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-07-14 14:16:57 -0400

    Added device registrations

--- src/HuddleRoom/ControlSystem.cs
+++ src/HuddleRoom/ControlSystem.cs
@@ -2,11 +2,22 @@ using System;
 using Crestron.SimplSharp;
 using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.DM.AirMedia;
+using Crestron.SimplSharpPro.DM.Endpoints.Transmitters;
+using Crestron.SimplSharpPro.GeneralIO;
 
 namespace HuddleRoom
 {
     public class ControlSystem : CrestronControlSystem
     {
+        private Am300 _dmRx;
+        private DmTx201C _dmTx;
+        private GlsOdtCCn _occSensor;
+
+        public bool HasAirMedia { get; private set; }
+        public bool HasLaptop { get; private set; }
+        public bool HasOccSensor { get; private set; }
+
         public ControlSystem()
             : base()
         {
@@ -22,9 +33,29 @@ namespace HuddleRoom
 
         public override void InitializeSystem()
         {
+            HasAirMedia = false;
+            HasLaptop = false;
+            HasOccSensor = false;
+
             try
             {
-                CrestronConsole.PrintLine("MaxNumberOfUserThreads = {0}", Thread.MaxNumberOfUserThreads);
+                if (this.SupportsEthernet)
+                {
+                    _dmRx = new Am300(0x15, this);
+                    if (_dmRx.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
+                        ErrorLog.Error("Unable to register {0} on IP ID {1}!", _dmRx.Name, _dmRx.ID);
+
+                    _dmTx = new DmTx201C(0x14, this);
+                    if (_dmTx.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
+                        ErrorLog.Error("Unable to register {0} on IP ID {1}!", _dmTx.Name, _dmTx.ID);
+                }
+
+                if (this.SupportsCresnet)
+                {
+                    _occSensor = new GlsOdtCCn(0x97, this);
+                    if (_occSensor.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
+                        ErrorLog.Error("Unable to register {0} on Cresnet ID {1}!", _occSensor.Name, _occSensor.ID);
+                }
             }
             catch (Exception e)
             {
